67d5dac9e1f9578f62acba0a89e5bba6cc670c30
Craig A. McDonald <craig@mcdonald.codes>
2020-10-01 20:48:41 +0100

    Initial commit

---
+++ src/main/java/codes/mcdonald/craig/todomvc/TodoSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package codes.mcdonald.craig.todomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codes/mcdonald/craig/todomvc/TodoSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.mcdonald.craig.todomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
