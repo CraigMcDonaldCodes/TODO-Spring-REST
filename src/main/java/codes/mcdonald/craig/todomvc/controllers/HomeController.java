5f5a04ecc974ff133dede0dde8f6320f061d61f6
Craig A. McDonald <craig@mcdonald.codes>
2020-10-08 22:14:13 +0100

    Minor updates

--- src/main/java/codes/mcdonald/craig/todomvc/controllers/HomeController.java
+++ src/main/java/codes/mcdonald/craig/todomvc/controllers/HomeController.java
@@ -1,5 +1,14 @@
 package codes.mcdonald.craig.todomvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String getHome() {
+		
+		return "home";
+	}
 }
--- src/main/java/codes/mcdonald/craig/todomvc/models/Entry.java
+++ src/main/java/codes/mcdonald/craig/todomvc/models/Entry.java
@@ -1,5 +1,12 @@
 package codes.mcdonald.craig.todomvc.models;
 
+import javax.persistence.Entity;
+
+import java.time.ZonedDateTime;
+
+@Entity
 public class Entry {
 
+	private String description;
+	private ZonedDateTime dateCreated;
 }
