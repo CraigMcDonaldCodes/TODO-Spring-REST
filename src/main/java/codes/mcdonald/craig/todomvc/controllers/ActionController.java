376d8971289bdf359fa9aa66e495d1e9473ee961
Craig A. McDonald <craig@mcdonald.codes>
2020-10-12 23:41:04 +0100

    REST controller, new mappings

--- src/main/java/codes/mcdonald/craig/todomvc/controllers/ActionController.java
+++ src/main/java/codes/mcdonald/craig/todomvc/controllers/ActionController.java
@@ -4,7 +4,11 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import codes.mcdonald.craig.todomvc.models.Entry;
@@ -12,6 +16,7 @@ import codes.mcdonald.craig.todomvc.models.Entry;
 @RestController
 public class ActionController {
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/api/entries")
 	public List<Entry> getEntries() {
 		
@@ -24,5 +29,44 @@ public class ActionController {
 		list.add(entry);
 		
 		return list;
+	}
+	
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	@GetMapping("/api/entry/{?}")
+	public Entry getEntry() {
+		
+		// TODO: Complete implementation
+
+		return new Entry();
+	}
+	
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	@PostMapping("/api/create")
+	public Entry createEntry() {
+		
+		// TODO: Complete implementation
+
+		return new Entry();
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	@RequestMapping(method = RequestMethod.PUT)
+	public Entry updateEntry() {
+
+		// TODO: Complete implementation
+		
+		return new Entry();
+	}
+	
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	@DeleteMapping("/api/delete")
+	public Entry deleteEntry() {
+		
+		// TODO: Complete implementation
+		
+		return new Entry();
 	}
 }
