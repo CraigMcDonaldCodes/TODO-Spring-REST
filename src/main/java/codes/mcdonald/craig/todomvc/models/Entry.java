f32c2f77d74cc68a5cd8ca1f94bf46d86ee21f31
Craig A. McDonald <craig@mcdonald.codes>
2020-10-25 22:58:16 +0000

    Entry entity id updated

--- src/main/java/codes/mcdonald/craig/todomvc/models/Entry.java
+++ src/main/java/codes/mcdonald/craig/todomvc/models/Entry.java
@@ -1,6 +1,8 @@
 package codes.mcdonald.craig.todomvc.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import java.time.ZonedDateTime;
@@ -9,6 +11,7 @@ import java.time.ZonedDateTime;
 public class Entry {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String description;
 	private ZonedDateTime dateCreated;
