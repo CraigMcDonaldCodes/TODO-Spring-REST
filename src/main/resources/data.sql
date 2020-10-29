bc5d0387f311f2e6ae999c8d878df9096b24e197
Craig A. McDonald <craig@mcdonald.codes>
2020-10-29 23:58:50 +0000

    SQL - added default values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,10 @@ CREATE TABLE "entries" (
 	date_created	TIMESTAMP		NOT NULL,
 	date_completed	TIMESTAMP		NULL
 );
+
+INSERT INTO "entries"
+	(description, date_created, date_completed)
+VALUES
+	('Read Java Performance by Scott Oaks', '2020-01-01 10:00:00', NULL),
+	('Read Java Performance by Scott Oaks', '2020-02-02 08:00:00', NULL),
+	('Read Java Performance by Scott Oaks', '2020-03-03 11:00:00', NULL);
