daf0c9697aa09ada5f521b4adce896157d82e9b3
Craig A. McDonald <craig@mcdonald.codes>
2020-10-29 21:42:23 +0000

    No time zone on the SQL data type

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS "entries";
 
 CREATE TABLE "entries" (
-	id				int							AUTO_INCREMENT PRIMARY KEY,
-	description		VARCHAR(200)				NOT NULL,
-	date_created	TIMESTAMP WITH TIME ZONE	NOT NULL,
-	date_completed	TIMESTAMP WITH TIME ZONE
+	id				int				AUTO_INCREMENT PRIMARY KEY,
+	description		VARCHAR(200)	NOT NULL,
+	date_created	TIMESTAMP		NOT NULL,
+	date_completed	TIMESTAMP		NULL
 );
