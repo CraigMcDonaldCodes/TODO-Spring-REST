ffaabdf29955be4f53a6ca476ee3c0bc1ff137f9
Craig A. McDonald <craig@mcdonald.codes>
2020-10-30 03:22:20 +0000

    Minor tweaks to UI and initial SQL data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,6 +10,7 @@ CREATE TABLE "entries" (
 INSERT INTO "entries"
 	(description, date_created, date_completed)
 VALUES
+	('Practice programming', '2020-01-01 10:00:00', NULL),
 	('Read Java Performance by Scott Oaks', '2020-01-01 10:00:00', NULL),
-	('Read Java Performance by Scott Oaks', '2020-02-02 08:00:00', NULL),
-	('Read Java Performance by Scott Oaks', '2020-03-03 11:00:00', NULL);
+	('Read Java Concurrent in Practice by Brian Goetz', '2020-02-02 08:00:00', NULL),
+	('Read Java Network Programming by Elliotte Rusty Harold', '2020-03-03 11:00:00', NULL);
