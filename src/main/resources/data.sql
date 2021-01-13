3f3c35b225b2a9738534fa0faf0f2b8a23f84bdd
Craig McDonald <craig@mcdonald.codes>
2021-01-13 17:06:27 +0000

    SQL - updated to create tables with quotes due to H2 oddity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS "entry";
+DROP TABLE IF EXISTS entry;
 
-CREATE TABLE "entry" (
-    id              int           AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE entry (
+    id              int           IDENTITY PRIMARY KEY,
     description     VARCHAR(200)  NOT NULL,
     date_created    TIMESTAMP     NOT NULL,
     date_completed  TIMESTAMP     NULL
 );
 
-INSERT INTO "entry"
+INSERT INTO entry
     (description, date_created, date_completed)
 VALUES
     ('Practice programming', '2020-01-01 10:00:00', NULL),
