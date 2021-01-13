74fee60a063768e663ecac2e3adf6773f19180ae
Craig McDonald <craig@mcdonald.codes>
2021-01-13 16:21:07 +0000

    SQL - table renamed to singular to match class

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS "entries";
+DROP TABLE IF EXISTS "entry";
 
-CREATE TABLE "entries" (
+CREATE TABLE "entry" (
     id              int           AUTO_INCREMENT PRIMARY KEY,
     description     VARCHAR(200)  NOT NULL,
     date_created    TIMESTAMP     NOT NULL,
     date_completed  TIMESTAMP     NULL
 );
 
-INSERT INTO "entries"
+INSERT INTO "entry"
     (description, date_created, date_completed)
 VALUES
     ('Practice programming', '2020-01-01 10:00:00', NULL),
