25b6e55afb12d6167403b55ba4973215c59f9a21
Craig McDonald <craig@mcdonald.codes>
2021-01-13 16:25:28 +0000

    Misc - no idea what changed but committing before next batch of changes

--- src/test/java/codes/mcdonald/craig/todorest/TodoSpringMvcApplicationTests.java
+++ src/test/java/codes/mcdonald/craig/todorest/TodoSpringMvcApplicationTests.java
@@ -1,13 +1,26 @@
-package codes.mcdonald.craig.todomvc;
+package codes.mcdonald.craig.todorest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import codes.mcdonald.craig.todorest.repository.TodoRepository;
 
 @SpringBootTest
 class TodoSpringMvcApplicationTests {
+	
+	@Autowired
+	private TodoRepository repository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void hibernateCreatesInitialDatabaseEntries() {
+		
+		var entries = repository.findAll();
+		Assertions.assertEquals(entries.size(), 4);
+	}
 }
