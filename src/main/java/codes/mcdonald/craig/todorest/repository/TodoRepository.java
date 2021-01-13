20292d41e1501b623b800f720cf0c8c0bc16e3ba
Craig McDonald <craig@mcdonald.codes>
2021-01-13 14:43:14 +0000

    Spring - change TodoRespository from CrudRepository to JpaRespository

--- src/main/java/codes/mcdonald/craig/todorest/repository/TodoRepository.java
+++ src/main/java/codes/mcdonald/craig/todorest/repository/TodoRepository.java
@@ -1,8 +1,8 @@
 package codes.mcdonald.craig.todorest.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import codes.mcdonald.craig.todorest.model.Entry;
 
-public interface TodoRepository extends CrudRepository<Entry, Long> {
+public interface TodoRepository extends JpaRepository<Entry, Long> {
 }
