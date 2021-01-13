1320fc9136b36f6eb82514d5677587d84af54891
Craig McDonald <craig@mcdonald.codes>
2021-01-13 17:39:19 +0000

    Tests - added failed test for checking that one new entry could be added to initial database

--- src/test/java/codes/mcdonald/craig/todorest/TodoSpringMvcApplicationTests.java
+++ src/test/java/codes/mcdonald/craig/todorest/TodoSpringMvcApplicationTests.java
@@ -1,10 +1,13 @@
 package codes.mcdonald.craig.todorest;
 
+import java.time.ZonedDateTime;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import codes.mcdonald.craig.todorest.model.Entry;
 import codes.mcdonald.craig.todorest.repository.TodoRepository;
 
 @SpringBootTest
@@ -22,5 +25,19 @@ class TodoSpringMvcApplicationTests {
 		
 		var entries = repository.findAll();
 		Assertions.assertEquals(entries.size(), 4);
+	}
+	
+	@Test
+	void testAddingOneNewEntryInInitialDatabase() {
+		
+		var entry = new Entry();
+		entry.setDescription("Create a test for basic TODO web application in Spring Boot");
+		entry.setDateCreated(ZonedDateTime.now());
+
+		repository.saveAndFlush(entry);
+		
+		var entries = repository.findAll();
+		
+		Assertions.assertEquals(entries.size(), 5);
 	}
 }
