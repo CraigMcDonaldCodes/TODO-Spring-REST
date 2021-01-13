5cea970a9a81481538a08feb92ac5841f6ec9cff
Craig McDonald <craig@mcdonald.codes>
2021-01-13 17:07:15 +0000

    Spring - added additional annotations for JPA entity

--- src/main/java/codes/mcdonald/craig/todorest/model/Entry.java
+++ src/main/java/codes/mcdonald/craig/todorest/model/Entry.java
@@ -1,20 +1,27 @@
 package codes.mcdonald.craig.todorest.model;
 
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import java.time.ZonedDateTime;
 
 @Entity
 public class Entry {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
+
+	@Column(length = 200)
 	private String description;
+
+	@Column(name = "date_created")
 	private ZonedDateTime dateCreated;
+
+	@Column(name = "date_completed")
 	private ZonedDateTime dateCompleted;
 
 	public long getId() {
