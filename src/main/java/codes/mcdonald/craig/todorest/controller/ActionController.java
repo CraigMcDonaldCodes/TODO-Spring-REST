8c2a1baf0a8f3b6fbb4cb3d599991007d201fd0e
Craig McDonald <craig@mcdonald.codes>
2021-01-14 22:18:22 +0000

    Spring - updated main controller to implement create on POST

--- src/main/java/codes/mcdonald/craig/todorest/controller/ActionController.java
+++ src/main/java/codes/mcdonald/craig/todorest/controller/ActionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,11 +40,9 @@ public class ActionController {
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	@PostMapping("/api/create")
-	public Entry createEntry() {
+	public Entry createEntry(@RequestBody Entry entry) {
 		
-		// TODO: Complete implementation
-
-		return new Entry();
+		return repository.save(entry);
 	}
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
