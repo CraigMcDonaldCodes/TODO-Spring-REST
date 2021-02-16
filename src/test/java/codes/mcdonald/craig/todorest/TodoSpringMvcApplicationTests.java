365f7f5a25ec5f82630d6691a5ea48fb87c42025
Craig A. McDonald <craig@mcdonald.codes>
2021-02-16 23:05:22 +0000

    Spring - added another test for the database

--- src/test/java/codes/mcdonald/craig/todorest/TodoSpringMvcApplicationTests.java
+++ src/test/java/codes/mcdonald/craig/todorest/TodoSpringMvcApplicationTests.java
@@ -1,6 +1,7 @@
 package codes.mcdonald.craig.todorest;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -41,5 +42,35 @@ class TodoSpringMvcApplicationTests {
 		var entries = repository.findAll();
 		
 		Assertions.assertEquals(5, entries.size());
+	}
+
+	@Test
+	void testAddingFourNewEntryInInitialDatabase() {
+		
+		var entry1 = new Entry();
+		entry1.setDescription("Read Java Concurrency in Practice");
+		entry1.setDateCreated(ZonedDateTime.now());
+		
+		var entry2 = new Entry();
+		entry2.setDescription("Read The Well Grounded Java Developer");
+		entry2.setDateCreated(ZonedDateTime.now());
+		
+		var entry3 = new Entry();
+		entry3.setDescription("Read Real World Software Development");
+		entry3.setDateCreated(ZonedDateTime.now());
+		
+		var entry4 = new Entry();
+		entry4.setDescription("Read Spring Security in Action");
+		entry4.setDateCreated(ZonedDateTime.now());
+		
+		List.of(entry1, entry2, entry3, entry4).
+			stream().
+			forEach(e -> repository.save(e));
+
+		repository.flush();
+		
+		var entries = repository.findAll();
+		
+		Assertions.assertEquals(8, entries.size());
 	}
 }
