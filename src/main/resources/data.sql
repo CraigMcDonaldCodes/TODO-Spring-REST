d2429abbd90b95e29435042d4065a313571315e4
Craig McDonald <craig@mcdonald.codes>
2021-04-26 23:15:27 +0100

    SQL - added two addition entries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,4 +13,6 @@ VALUES
     ('Practice programming', '2020-01-01 10:00:00', NULL),
     ('Read Java Performance by Scott Oaks', '2020-01-01 10:00:00', NULL),
     ('Read Java Concurrent in Practice by Brian Goetz', '2020-02-02 08:00:00', NULL),
-    ('Read Java Network Programming by Elliotte Rusty Harold', '2020-03-03 11:00:00', NULL);
+    ('Read Java Network Programming by Elliotte Rusty Harold', '2020-03-03 11:00:00', NULL),
+    ('Read Spring in Action by Craig Walls', '2021-02-01 10:00:00', NULL),
+    ('Read Spring Security in Action by Laurențiu Spilcă', '2021-03-20 08:54:00', NULL);
