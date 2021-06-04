391dc1817ed807060841a78ca99e2344033b0b76
Craig McDonald <craig@mcdonald.codes>
2021-06-04 23:11:24 +0100

    UI - removed current in preparation for React

--- src/main/resources/static/site.js
+++
@@ -1,9 +0,0 @@
-// Handler for input submitted
-$(document).on('click', '#submit', function(e) {
-
-	var msg = $("#entry-input").val();
-	console.log("Input value: " + msg);
-
-	// Clear the input
-	$("#entry-input").val("");
-});
